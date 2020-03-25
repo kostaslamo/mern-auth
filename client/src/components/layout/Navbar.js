41e58bfe91ae930e694d47e366fbf6b51e855633
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2020-03-25 11:26:03 +0200

    changed navbar title

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
               }}
               className="col s5 brand-logo center black-text"
             >
-              ICE ADMIN
+              ADMIN
             </Link>
           </div>
         </nav>
